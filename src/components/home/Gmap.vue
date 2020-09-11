d94842011a54820e9b87a6ea3e761370e9ec7bd6
chickenfoot88 <chikenfoot88@gmail.com>
2020-09-11 11:09:28 +0300

    Add gmap page

---
+++ src/components/home/Gmap.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="map">
+    <h2>Map</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Gmap',
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Gmap from '../components/home/Gmap.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Gmap',
+    component: Gmap
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+
 ]
 
 const router = new VueRouter({
