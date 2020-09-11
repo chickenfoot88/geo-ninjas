4037793b677f0e4c18d313121a704474f02e7cb7
chickenfoot88 <chikenfoot88@gmail.com>
2020-09-11 21:13:05 +0300

    Add firebase auth before app init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+let app = null
+
+// wait for firebase auth to init before creating the app
+firebase.auth().onAuthStateChanged(() => {
+
+  // init app if not already created
+  if (app) return
+  
+  new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app')
+})
+
