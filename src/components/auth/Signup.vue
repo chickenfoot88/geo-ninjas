e1f90c6df3d400faf493c2c2876e51f3cc17482a
chickenfoot88 <chikenfoot88@gmail.com>
2020-09-11 21:03:41 +0300

    Add sign up page

--- src/components/auth/Signup.vue
+++ src/components/auth/Signup.vue
@@ -14,6 +14,7 @@
         <label for="alias">Alias:</label>
         <input type="text" name="alias" v-model="alias"/>
       </div>
+      <p v-if="feedback" class="red-text center">{{ feedback }}</p>
       <div class="field center">
         <button class="btn deep-purple">Sign up</button>
       </div>
@@ -21,18 +22,56 @@
   </div>
 </template>
 <script>
+import slugify from 'slugify'
+import db from '@/firebase/init'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
 export default {
   name: 'Signup',
   data() {
     return {
       email: '',
       password: '',
-      alias: ''
+      alias: '',
+      feedback: '',
+      slug: ''
     }
   },
   methods: {
-    signUp() {
-      
+    async signUp() {
+      if (this.alias && this.email && this.password) {
+        this.slug = slugify(this.alias, {
+          replacement: '-',
+          remove: /[$*_+~.()'"!\-:@]/g,
+          lower: true
+        })
+
+        const usersRef = db.collection('users').doc(this.slug)
+        try {
+          const doc = await usersRef.get()
+
+          if (doc.exists) {
+            this.feedback = 'This alias allready exists'
+          } else {
+            const { user } = await firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+            usersRef.set({
+              alias: this.alias,
+              geolocation: null,
+              user_id: user.uid
+            })
+
+            this.$router.push({ name: 'Gmap' })
+            this.feedback = ''
+          }
+        } catch (error) {
+          console.error(error);
+          this.feedback = error.message
+        }
+
+      } else {
+        this.feedback = 'You must enter all fields'
+      }
     }
   }
 }
