ab944cb78ddd5c55a91a64a884351e5e871e03cd
chickenfoot88 <chikenfoot88@gmail.com>
2020-09-11 11:55:03 +0300

    Add google map

--- src/components/home/Gmap.vue
+++ src/components/home/Gmap.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="map">
-    <h2>Map</h2>
+    <div class="google-map" id="map" ref="map"></div>
   </div>
 </template>
 <script>
@@ -8,9 +8,36 @@ export default {
   name: 'Gmap',
   data() {
     return {
-      
+      lat: 53,
+      lng: -2
+    }
+  },
+  mounted() {
+    this.renderMap()
+  },
+  methods: {
+    renderMap() {
+      const mapEl = this.$refs.map
+      // eslint-disable-next-line no-undef, no-unused-vars
+      const map = new google.maps.Map(mapEl, {
+        center: { lat: this.lat, lng: this.lng },
+        zoom: 6,
+        maxZoom: 15,
+        minZoom: 3,
+        streetViewControl: false
+      })
     }
   }
 }
 </script>
-<style></style>
+<style>
+.google-map {
+  width: 100%;
+  height: 100%;
+  margin: 0 auto;
+  background: #fff;
+  position: absolute;
+  right: 0;
+  left: 0;
+}
+</style>
